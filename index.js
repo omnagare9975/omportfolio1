c0fc1df4dd75988d87bfe87419ef16346b25957a
Om Nagare <118531541+omnagare9975@users.noreply.github.com>
2023-09-01 16:15:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const element = document.getElementById("#csbutton");
+element.addEventListener("click", function() {
+  document.getElementById("#demo").innerHTML = "Sorry...I am Still Working On it.."
+});
